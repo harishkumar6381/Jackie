d8e2ff0e5d7e1414e61940e47e8bdd4ea9e5ebcd
harishkumar6381 <emvharish20@gmail.com>
2022-02-03 18:25:23 +0530

    folder structure

---
+++ src/org/phone/PhoneInfo.java
@@ -0,0 +1,27 @@
+package org.phone;
+
+public class PhoneInfo {
+        private void phoneName() {
+		    System.out.println("phone name is realme");
+	    }
+        private void phoneMieiNum() {
+			System.out.println("phone miei number is 9876543210");
+		}
+        private void Camera() {
+			System.out.println("camera is 64mp");
+		}
+        private void storage() {
+			System.out.println("storage is 128gb");
+		}
+        private void osName() {
+			System.out.println("os name is 10 pie");
+		}
+        public static void main(String[] args) {
+			PhoneInfo p = new PhoneInfo();
+			p.phoneName();
+			p.phoneMieiNum();
+			p.Camera();
+			p.storage();
+			p.osName();
+		}
+}
